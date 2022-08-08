1ee2a2be97def53d32c23e109ae27c1722ae2891
TehTrooper <oskar.game@outlook.com>
2022-08-08 22:56:14 +0200

    Update morselibrary.py

--- morselibrary.py
+++ morselibrary.py
@@ -1,5 +1,5 @@
 class morseLibrary:
-    __morseLibrary = {
+    __library = {
         "A" : ".-",
         "B" : "-...",
         "C" : "-.-.",
@@ -56,15 +56,37 @@ class morseLibrary:
         "@" : ".--.-."
     }
 
+    # Returns the dictionary used in the class
+    def getMorse(self):
+        return self.__library
+
+    # Returns True if the character exist
+    def doesCharExist(self, char):
+        if char[len(char) - 1] == " ":
+            char = char[:len(char)-1]
+        for i in morseLibrary.__library.keys():
+            if i == char.capitalize():
+                return True
+        return False
+
+    def doesMorseCodeExist(self, code):
+        if code[len(code) - 1] == " ":
+            code = code[:len(code)-1]
+        for char, morse in morseLibrary().__library.items():
+            if morse == code:
+                return True
+            
+        return False
+
     # Returns the corresponding morse code from a single character
-    def returnMorse(self, Character):
+    def returnMorse(self, char):
         
-        return morseLibrary.__morseLibrary.get(Character.capitalize())
+        return morseLibrary.__library.get(char.capitalize())
     
     # Returns the corresponding character from a string of morse
     def returnChar(self, code):
 
-        for char, morse in morseLibrary.__morseLibrary.items():
+        for char, morse in morseLibrary.__library.items():
             if morse == code:
                 return char
         # return None
@@ -83,19 +105,20 @@ class morseLibrary:
 
     # Transklates a entire morse code to a spring
     def translateToSentence(self, code):
-
+        if code[len(code) - 1] != " ":
+            code += " "
         sentence = ""
         morse = ""
 
         for char in code:
-
             
             if char == "/":
                 sentence += " "
-            elif char == " " and morse != "":
-                sentence += str(self.returnChar(morse))
-                morse = ""
+            elif char == " ":
+                if morse != "":
+                    sentence += str(self.returnChar(morse))
+                    morse = ""
             elif char != " ":
                 morse += char
-
+            
         return sentence
